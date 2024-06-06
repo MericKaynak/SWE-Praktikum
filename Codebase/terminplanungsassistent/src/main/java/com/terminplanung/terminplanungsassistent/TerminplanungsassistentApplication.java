158b2b7d6fa6d39e28df137bfe6fc3ded99ea3f4
alexander foerster <alexander.foerster@stud.hn.de>
2024-06-06 21:08:59 +0200

    spring initializr + flyway integration

---
+++ Codebase/terminplanungsassistent/src/main/java/com/terminplanung/terminplanungsassistent/TerminplanungsassistentApplication.java
@@ -0,0 +1,13 @@
+package com.terminplanung.terminplanungsassistent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminplanungsassistentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerminplanungsassistentApplication.class, args);
+	}
+
+}
---
+++ Codebase/terminplanungsassistent/src/test/java/com/terminplanung/terminplanungsassistent/TerminplanungsassistentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terminplanung.terminplanungsassistent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerminplanungsassistentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
