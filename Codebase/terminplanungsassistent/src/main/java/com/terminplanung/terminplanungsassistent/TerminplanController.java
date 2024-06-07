94b062b1fd25042af1f5f515382b89ce27445197
alexander foerster <alexander.foerster@stud.hn.de>
2024-06-07 14:26:06 +0200

    docker + RESTController setup + flyway integrated

---
+++ Codebase/terminplanungsassistent/src/main/java/com/terminplanung/terminplanungsassistent/TerminplanController.java
@@ -0,0 +1,10 @@
+package com.terminplanung.terminplanungsassistent;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/terminplan")
+public class TerminplanController {
+
+}
